14c3dd540bc6fc962c2bc44a96c17e12099b43ae
Appleyard Vincent <appleyardvincent@gmail.com>
2021-08-18 18:55:46 +0100

    updated realistic ponzi

--- contracts/realistic_ponzi.sol
+++ contracts/realistic_ponzi.sol
@@ -1,28 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/*
+each time an new investor invest, old investors receives
+a share of the investment
+*/
+
 contract GradualPonzi {
     address[] public investors;
     mapping(address => uint256) public balances;
+    // MINIMUM_INVESTMENT = 10^15 wei
     uint256 public constant MINIMUM_INVESTMENT = 1e15;
 
     constructor() {
         investors.push(msg.sender);
     }
 
-    fallback() external payable {
-        require(msg.value >= MINIMUM_INVESTMENT);
+    receive() external payable {
+        require(
+            msg.value >= MINIMUM_INVESTMENT,
+            "amount is less than MINIMUM_INVESTMENT"
+        );
 
         uint256 eachInvestorGets = msg.value / investors.length;
 
+        // update investors balances
         for (uint256 i = 0; i < investors.length; i++) {
             balances[investors[i]] += eachInvestorGets;
         }
-        
+
         investors.push(msg.sender);
     }
-
-    receive() external payable {}
 
     function withdraw() public {
         uint256 payout = balances[msg.sender];
