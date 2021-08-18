1f911cd802ab857124a9fbb0227cf6e7eb37077f
Appleyard Vincent <appleyardvincent@gmail.com>
2021-08-18 17:20:09 +0100

    updated simplepozi contract

--- contracts/simple_ponzi.sol
+++ contracts/simple_ponzi.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/*
+The simplest version of a Ponzi involves taking the money sent by the 
+current investor and transfering it to the previous investor. As long as 
+each investment is larger than the previous one, every investor except the 
+last will get a return on their investment.
+*/
+
 contract SimplePonzi {
     address public currentInvestor;
     uint256 public currentInvestment = 0;
 
-    fallback() external payable {
+    receive() external payable {
         // new investments must be 10% greater than current investment
-        uint256 minimumInvestment = (currentInvestment * 1) / 10;
+        uint256 minimumInvestment = (currentInvestment * 11) / 10;
         require(msg.value > minimumInvestment);
 
         // document new investor
@@ -18,7 +25,4 @@ contract SimplePonzi {
         // payout previous investor
         previousInvestor.transfer(msg.value);
     }
-
-    receive() external payable {}
-
 }
